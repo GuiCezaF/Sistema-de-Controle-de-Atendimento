ceed9cb9d8cf2e86893589f616ff8d0883876254
GuiCezaF <contato.guicezafe@gmail.com>
2024-04-13 20:41:40 -0300

    feat: adicionado .env para segurança nas conexoes

--- src/main/conexoes/ConexaoMySQL.java
+++ src/main/conexoes/ConexaoMySQL.java
@@ -1,16 +1,16 @@
 package main.conexoes;
 
-
+import io.github.cdimascio.dotenv.Dotenv;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class ConexaoMySQL {
-
+    Dotenv dotenv = Dotenv.load(); // Carrega o arquivo .env na raiz do projeto
     private Connection con;
     private String driver = "com.mysql.cj.jdbc.Driver";
-    private String url = "jdbc:mysql://localhost:3306/db";
-    private String user = "root";
-    private String password = "superSenhaSecreta";
+    private String url = dotenv.get("MYSQL_URL");
+    private String user = dotenv.get("MYSQL_USER");
+    private String password = dotenv.get("MYSQL_PASSWORD");
 
     public Connection conectar(){
         try {
