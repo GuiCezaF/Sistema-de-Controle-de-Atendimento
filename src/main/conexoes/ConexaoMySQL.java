753b7861bd568beaaa97d1571a239fdf26a276bb
GuiCezaF <contato.guicezafe@gmail.com>
2024-04-13 20:28:33 -0300

    feat: adicionado composer com o banco mysql 8.0.35. + Classe de conexão com o banco e classe para testar a conexão com o banco

--- src/main/conexoes/ConexaoMySQL.java
+++ src/main/conexoes/ConexaoMySQL.java
@@ -1,9 +1,30 @@
 package main.conexoes;
 
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class ConexaoMySQL {
 
-    //TODO (Guilherme) - Criar a conexão com o banco de dados
+    private Connection con;
+    private String driver = "com.mysql.cj.jdbc.Driver";
+    private String url = "jdbc:mysql://localhost:3306/db";
+    private String user = "root";
+    private String password = "superSenhaSecreta";
+
+    public Connection conectar(){
+        try {
+            Class.forName(driver);
+            con = DriverManager.getConnection(url, user, password);
+            return con;
+        }catch (Exception e){
+            System.out.println(e);
+            return null;
+        }
+    }
+
+
 }
+
 
 
---
+++ src/main/conexoes/testeDb.java
@@ -0,0 +1,28 @@
+package main.conexoes;
+
+import main.conexoes.ConexaoMySQL;
+
+import java.sql.Connection;
+
+public class testeDb {
+    public static void main(String[] args) {
+        status();
+
+    }
+
+    private static void status(){
+        Connection con;
+        ConexaoMySQL db = new ConexaoMySQL();
+        try {
+            con = db.conectar();
+            if(con == null){
+                System.out.println("Erro de Conexão!");
+            }else {
+                System.out.println("Banco conectado...");
+            }
+            con.close();
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+}
