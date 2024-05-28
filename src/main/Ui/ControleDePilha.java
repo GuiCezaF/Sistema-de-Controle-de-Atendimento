fc24ccfe3239eff6d0abfe5688a1742614a6d1f0
GuiCezaF <contato.guicezafe@gmail.com>
2024-05-27 21:27:24 -0300

    feat: adicionando o funcionamento de gerar uma senha ao apertar o botão e chamando a proxima senha

--- src/main/Ui/ControleDePilha.java
+++ src/main/Ui/ControleDePilha.java
@@ -1,6 +1,11 @@
 package main.Ui;
 
+import main.Pilha;
+import main.TipoLista;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ControleDePilha extends JFrame{
     private JPanel uiPanel;
@@ -11,11 +16,11 @@ public class ControleDePilha extends JFrame{
     private JButton BtnPfl;
     private JButton BtnUrg;
     private JButton BtnChamaProximo;
-    private JLabel lblSenhaAtual;
+    private JLabel lblSuaSenha;
     private JLabel lblSenhaAntiga1;
     private JLabel lblSenhaAntiga2;
     private JLabel lblSenhaAntiga3;
-    private JLabel lblSuaSenha;
+    private JLabel lblSenhaAtual;
 
     public ControleDePilha(){
         setContentPane(uiPanel);
@@ -24,9 +29,88 @@ public class ControleDePilha extends JFrame{
         setSize(800, 400);
         setLocationRelativeTo(null);
         setVisible(true);
+
+        Pilha pilhaUrgente = new Pilha(TipoLista.URGENTE);
+        Pilha pilhaIdoso80 = new Pilha(TipoLista.IDOSO80);
+        Pilha pilhaIdoso60 = new Pilha(TipoLista.IDOSO);
+        Pilha pilhaPreferencial = new Pilha(TipoLista.PREFERENCIAL);
+        Pilha pilhaVIP = new Pilha(TipoLista.VIP);
+        Pilha pilhaNormal = new Pilha(TipoLista.NORMAL);
+
+
+        BtnUrg.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String senhaAtual = pilhaUrgente.inserir();
+                lblSuaSenha.setText(senhaAtual);
+            }
+        });
+
+        BtnI80.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String senhaAtual = pilhaIdoso80.inserir();
+                lblSuaSenha.setText(senhaAtual);
+            }
+        });
+        BtnI60.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String senhaAtual = pilhaIdoso60.inserir();
+                lblSuaSenha.setText(senhaAtual);
+            }
+        });
+        BtnPfl.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String senhaAtual = pilhaPreferencial.inserir();
+                lblSuaSenha.setText(senhaAtual);
+            }
+        });
+        BtnVip.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String senhaAtual = pilhaVIP.inserir();
+                lblSuaSenha.setText(senhaAtual);
+            }
+        });
+        BtnNml.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String senhaAtual = pilhaNormal.inserir();
+                lblSuaSenha.setText(senhaAtual);
+            }
+        });
+
+        BtnChamaProximo.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String chamar = chamarProximaSenha(pilhaUrgente, pilhaIdoso80, pilhaIdoso60, pilhaPreferencial, pilhaVIP, pilhaNormal);
+                lblSenhaAtual.setText(chamar);
+
+            }
+        });
     }
 
     public static void main(String[] args) {
         new ControleDePilha();
+    }
+
+    public static String chamarProximaSenha(Pilha... pilhas) {
+        for (TipoLista tipo : getOrdemPrioridade()) {
+            for (Pilha pilha : pilhas) {
+                if (pilha.getTipoLista() == tipo) {
+                    String chamada = pilha.chamar();
+                    if (!chamada.equals("Nenhuma senha disponível.")) {
+                        return chamada;
+                    }
+                }
+            }
+        }
+        return "Nenhuma senha disponível.";
+    }
+
+    public static TipoLista[] getOrdemPrioridade() {
+        return new TipoLista[]{TipoLista.URGENTE, TipoLista.IDOSO80, TipoLista.IDOSO, TipoLista.PREFERENCIAL, TipoLista.VIP, TipoLista.NORMAL};
     }
 }
